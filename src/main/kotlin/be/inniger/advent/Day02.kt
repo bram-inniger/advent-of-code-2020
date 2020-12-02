a438ebaf4505182b88efd06f961898851353ee36
Bram Inniger <bram@inniger.be>
2020-12-02 10:34:38 +0200

    D02P02 - Solved

--- src/main/kotlin/be/inniger/advent/Day02.kt
+++ src/main/kotlin/be/inniger/advent/Day02.kt
@@ -4,25 +4,38 @@ class Day02 {
 
     fun solveFirst(passwords: List<String>) = passwords
         .map { Password.of(it) }
-        .filter { it.isValid() }
+        .filter { it.isValidWithOldPolicy() }
         .count()
 
-    private data class Password(val policyMin: Int, val policyMax: Int, val letter: Char, val plainText: String) {
+    fun solveSecond(passwords: List<String>) = passwords
+        .map { Password.of(it) }
+        .filter { it.isValidWithNewPolicy() }
+        .count()
+
+    private data class Password(val lower: Int, val upper: Int, val letter: Char, val plainText: String) {
 
         companion object {
             private val regex = """^(\d+)-(\d+) ([a-z]): ([a-z]+)$""".toRegex()
 
             fun of(password: String): Password {
-                val (policyMin, policyMax, letter, plainText) = regex.find(password)!!.destructured
+                val (lower, upper, letter, plainText) = regex.find(password)!!.destructured
 
-                return Password(policyMin.toInt(), policyMax.toInt(), letter.single(), plainText)
+                return Password(lower.toInt(), upper.toInt(), letter.single(), plainText)
             }
         }
 
-        fun isValid() = plainText
+        fun isValidWithOldPolicy() = plainText
             .toCharArray()
             .filter { it == letter }
             .count()
-            .let { it in policyMin..policyMax }
+            .let { it in lower..upper }
+
+        fun isValidWithNewPolicy(): Boolean {
+            val first = plainText[lower - 1]
+            val second = plainText[upper - 1]
+
+            return (first == letter && second != letter) ||
+                    (first != letter && second == letter)
+        }
     }
 }
--- src/test/kotlin/be/inniger/advent/Day02Test.kt
+++ src/test/kotlin/be/inniger/advent/Day02Test.kt
@@ -8,23 +8,29 @@ class Day02Test {
 
     private val problem = Day02()
     private val input = readInputFile("02")
+    private val passwords = listOf(
+        "1-3 a: abcde",
+        "1-3 b: cdefg",
+        "2-9 c: ccccccccc"
+    )
 
     @Test
     fun validateFirstSampleInputs() {
-        assertEquals(
-            2,
-            problem.solveFirst(
-                listOf(
-                    "1-3 a: abcde",
-                    "1-3 b: cdefg",
-                    "2-9 c: ccccccccc"
-                )
-            )
-        )
+        assertEquals(2, problem.solveFirst(passwords))
     }
 
     @Test
     fun validateFirstSolution() {
-        assertEquals(0, problem.solveFirst(input))
+        assertEquals(600, problem.solveFirst(input))
+    }
+
+    @Test
+    fun validateSecondSampleInputs() {
+        assertEquals(1, problem.solveSecond(passwords))
+    }
+
+    @Test
+    fun validateSecondSolution() {
+        assertEquals(245, problem.solveSecond(input))
     }
 }
