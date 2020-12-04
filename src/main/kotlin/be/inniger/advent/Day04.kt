ea6e30ebe206969deb3972f659d44cb045cfa996
Bram Inniger <bram@inniger.be>
2020-12-04 11:19:43 +0200

    D04P02 - Solved

--- src/main/kotlin/be/inniger/advent/Day04.kt
+++ src/main/kotlin/be/inniger/advent/Day04.kt
@@ -5,30 +5,47 @@ class Day04 {
     fun solveFirst(passportsDescription: String) =
         passportsDescription.split("\n\n")
             .map { Passport.of(it) }
-            .filter { it.isValid() }
+            .filter { it.isWeaklyValid() }
             .count()
 
-    private data class Passport(val fields: Set<String>) {
+    fun solveSecond(passportsDescription: String) =
+        passportsDescription.split("\n\n")
+            .map { Passport.of(it) }
+            .filter { it.isStronglyValid() }
+            .count()
+
+    private data class Passport(val fields: Map<String, String>) {
 
         companion object {
-            private val requiredFields = listOf(
-                "byr",
-                "iyr",
-                "eyr",
-                "hgt",
-                "hcl",
-                "ecl",
-                "pid"
+            private val rules = listOf(
+                Rule("byr") { it.length == 4 && it.toInt() in 1920..2002 },
+                Rule("iyr") { it.length == 4 && it.toInt() in 2010..2020 },
+                Rule("eyr") { it.length == 4 && it.toInt() in 2020..2030 },
+                Rule("hgt") {
+                    when (it.substring(it.length - 2)) {
+                        "cm" -> it.substringBefore("cm").toInt() in 150..193
+                        "in" -> it.substringBefore("in").toInt() in 59..76
+                        else -> false
+                    }
+                },
+                Rule("hcl") { """^#[0-9a-f]{6}$""".toRegex().containsMatchIn(it) },
+                Rule("ecl") { setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth").contains(it) },
+                Rule("pid") { """^\d{9}$""".toRegex().containsMatchIn(it) }
             )
 
             internal fun of(passportDescription: String) =
                 passportDescription.replace('\n', ' ')
                     .split(' ')
-                    .map { it.substringBefore(':') }
-                    .toSet()
+                    .map { it.substringBefore(':') to it.substringAfter(':') }
+                    .toMap()
                     .let { Passport(it) }
         }
 
-        fun isValid() = requiredFields.all { fields.contains(it) }
+        fun isWeaklyValid() = rules.map { it.field }.all { fields.containsKey(it) }
+
+        fun isStronglyValid() =
+            rules.all { it.validator.invoke(fields.getOrDefault(it.field, "INVALID")) }
     }
+
+    private data class Rule(val field: String, val validator: (String) -> Boolean)
 }
