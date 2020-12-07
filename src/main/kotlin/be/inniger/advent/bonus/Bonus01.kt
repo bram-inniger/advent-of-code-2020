e8a06947b79b607f45fc8a89a9db358c7d0f8bc1
Bram Inniger <bram@inniger.be>
2020-12-07 21:13:40 +0200

    B01P02 - Solved

--- src/main/kotlin/be/inniger/advent/bonus/Bonus01.kt
+++ src/main/kotlin/be/inniger/advent/bonus/Bonus01.kt
@@ -5,8 +5,13 @@ import kotlin.math.sqrt
 
 class Bonus01 {
 
-    fun solveFirst(population: Int) =
-        getMinimalSideLength(population)
+    fun solveFirst(population: Long) =
+        computeMinimalSideLength(population)
+
+    fun solveSecond(populations: Collection<Long>) =
+        populations.map { computeMinimalSideLength(it) }
+            .map { computePiecesOfFabric(it) }
+            .sum()
 
     /*
      * Using pen and paper it is clear that, for a given `n` number of sides,
@@ -16,6 +21,12 @@ class Bonus01 {
      * As a reminder, for an equation `a*x^2 + b*x + c = 0`,
      * the solution is in the form `(-b +- sqrt(b^2 - 4*a*c)) / (2*a)
      */
-    private fun getMinimalSideLength(population: Int) =
+    private fun computeMinimalSideLength(population: Long) =
         ceil((2.0 + sqrt(4.0 + 28.0 * population)) / 14).toInt()
+
+    /*
+     * It's fairly obvious fairly quick that `nrPiecesOfFabric` required equals 8 times the side length
+     */
+    private fun computePiecesOfFabric(sideLength: Int) =
+        8 * sideLength
 }
--- src/test/kotlin/be/inniger/advent/bonus/Bonus01Test.kt
+++ src/test/kotlin/be/inniger/advent/bonus/Bonus01Test.kt
@@ -17,8 +17,29 @@ class Bonus01Test {
 
     @Test
     fun validateFirstSolution() {
-        val netherlandsPopulation = 17_479_043
+        val netherlandsPopulation = 17_479_043L
 
         assertEquals(1581, problem.solveFirst(netherlandsPopulation))
+    }
+
+    @Test
+    fun validateSecondSampleInputs() {
+        assertEquals(8, problem.solveSecond(listOf(5)))
+        assertEquals(32, problem.solveSecond(listOf(104)))
+        assertEquals(200, problem.solveSecond(listOf(4_325)))
+    }
+
+    @Test
+    fun validateSecondSolution() {
+        val populations = mapOf<String, Long>(
+            "Asia" to 4_541_609_892,
+            "Africa" to 1_340_869_106,
+            "Europe" to 747_751_712,
+            "South America" to 430_857_328,
+            "North America" to 368_983_009,
+            "Oceania" to 42_705_675
+        )
+
+        assertEquals(537816, problem.solveSecond(populations.values))
     }
 }
