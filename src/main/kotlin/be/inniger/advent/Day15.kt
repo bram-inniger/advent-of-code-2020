1bf314e5b5299f622ec7c9189642b5d536951666
Bram Inniger <bram@inniger.be>
2020-12-15 12:13:25 +0200

    D15P02 - Solved

--- src/main/kotlin/be/inniger/advent/Day15.kt
+++ src/main/kotlin/be/inniger/advent/Day15.kt
@@ -2,18 +2,34 @@ package be.inniger.advent
 
 class Day15 {
 
-    companion object {
-        private const val SPOKEN_INDEX = 2020
-    }
+    fun solveFirst(startingNumbers: List<Int>): Int {
+        val maxTurn = 2020
 
-    fun solveFirst(startingNumbers: List<Int>) =
-        speak(
-            1 + startingNumbers.size,
+        tailrec fun speak(turn: Int, lastSpoken: Int, history: Map<Int, Int>): Int =
+            if (turn == maxTurn) lastSpoken
+            else speak(turn + 1, turn - (history[lastSpoken] ?: turn), history + (lastSpoken to turn))
+
+        return speak(
+            startingNumbers.size + 1,
             0,
             startingNumbers.mapIndexed { index, number -> number to index + 1 }.toMap()
         )
+    }
 
-    private tailrec fun speak(turn: Int, lastSpoken: Int, history: Map<Int, Int>): Int =
-        if (turn == SPOKEN_INDEX) lastSpoken
-        else speak(turn + 1, turn - (history[lastSpoken] ?: turn), history + (lastSpoken to turn))
+    fun solveSecond(startingNumbers: List<Int>): Int {
+        val maxTurn = 30_000_000
+        val history = startingNumbers.mapIndexed { index, number -> number to index + 1 }.toMap().toMutableMap()
+
+        var turn = startingNumbers.size + 1
+        var lastSpoken = 0
+
+        while (turn < maxTurn) {
+            val newLastSpoken = turn - (history[lastSpoken] ?: turn)
+            history[lastSpoken] = turn
+            lastSpoken = newLastSpoken
+            turn++
+        }
+
+        return lastSpoken
+    }
 }
--- src/test/kotlin/be/inniger/advent/Day15Test.kt
+++ src/test/kotlin/be/inniger/advent/Day15Test.kt
@@ -24,4 +24,20 @@ class Day15Test {
     fun validateFirstSolution() {
         assertEquals(257, problem.solveFirst(input))
     }
+
+    @Test
+    fun validateSecondSampleInputs() {
+        assertEquals(175_594, problem.solveSecond(listOf(0, 3, 6)))
+        assertEquals(2_578, problem.solveSecond(listOf(1, 3, 2)))
+        assertEquals(3_544_142, problem.solveSecond(listOf(2, 1, 3)))
+        assertEquals(261_214, problem.solveSecond(listOf(1, 2, 3)))
+        assertEquals(6_895_259, problem.solveSecond(listOf(2, 3, 1)))
+        assertEquals(18, problem.solveSecond(listOf(3, 2, 1)))
+        assertEquals(362, problem.solveSecond(listOf(3, 1, 2)))
+    }
+
+    @Test
+    fun validateSecondSolution() {
+        assertEquals(8_546_398, problem.solveSecond(input))
+    }
 }
