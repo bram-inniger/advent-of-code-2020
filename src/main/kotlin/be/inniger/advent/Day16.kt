d3d5e630f532782b66dea1d8a94fc76b7a4ef9aa
Bram Inniger <bram@inniger.be>
2020-12-16 17:53:24 +0200

    D16P02 - Solved

--- src/main/kotlin/be/inniger/advent/Day16.kt
+++ src/main/kotlin/be/inniger/advent/Day16.kt
@@ -8,11 +8,53 @@ class Day16 {
         val ranges = Field.allOf(fieldsRaw)
             .flatMap { listOf(it.range1, it.range2) }
 
-        return Ticket.allNearbyOf(nearbyTicketsRaw)
+        return Ticket.allOf(nearbyTicketsRaw)
             .flatMap { it.values }
             .filter { value -> ranges.none { range -> range.contains(value) } }
             .sum()
     }
+
+    fun solveSecond(notes: String): Long {
+        val (fieldsRaw, yourTicketRaw, nearbyTicketsRaw) = notes.split("\n\n")
+
+        val fields = Field.allOf(fieldsRaw)
+        val ranges = fields
+            .flatMap { listOf(it.range1, it.range2) }
+        val yourTicket = Ticket.of(yourTicketRaw)
+        val validNearbyTickets = Ticket.allOf(nearbyTicketsRaw)
+            .filter { it.values.all { value -> ranges.any { range -> range.contains(value) } } }
+
+        return findPositions(fields, validNearbyTickets)
+            .filter { it.field.name.startsWith("departure") }
+            .map { it.position }
+            .map { yourTicket.values[it].toLong() }
+            .reduce(Long::times)
+    }
+
+    private tailrec fun findPositions(
+        fields: List<Field>,
+        tickets: List<Ticket>,
+        toFind: Set<Int> = fields.indices.toSet(),
+        found: List<Result> = listOf()
+    ): List<Result> =
+        if (toFind.isEmpty()) found
+        else {
+            val result = findNextUniquePosition(toFind, fields, tickets)
+            findPositions(fields, tickets, toFind - result.position, found + result)
+        }
+
+    private fun findNextUniquePosition(toFind: Set<Int>, fields: List<Field>, tickets: List<Ticket>) =
+        fields
+            .map { field -> field to findAllMatchingPositions(toFind, tickets, field) }
+            .single { it.second.size == 1 }
+            .let { Result(it.first, it.second.single()) }
+
+    private fun findAllMatchingPositions(toFind: Set<Int>, tickets: List<Ticket>, field: Field) =
+        toFind.filter { position ->
+            tickets.all { ticket ->
+                field.range1.contains(ticket.values[position]) || field.range2.contains(ticket.values[position])
+            }
+        }
 
     private data class Field(val name: String, val range1: IntRange, val range2: IntRange) {
 
@@ -36,11 +78,20 @@ class Day16 {
     private data class Ticket(val values: List<Int>) {
 
         companion object {
-            fun allNearbyOf(tickets: String): List<Ticket> =
+            fun of(ticket: String): Ticket =
+                ticket.split("\n")
+                    .filter { it != "your ticket:" }
+                    .map { it.split(",").map(String::toInt) }
+                    .map { Ticket(it) }
+                    .single()
+
+            fun allOf(tickets: String): List<Ticket> =
                 tickets.split("\n")
                     .filter { it != "nearby tickets:" }
                     .map { it.split(",").map(String::toInt) }
                     .map { Ticket(it) }
         }
     }
+
+    private data class Result(val field: Field, val position: Int)
 }
--- src/test/kotlin/be/inniger/advent/Day16Test.kt
+++ src/test/kotlin/be/inniger/advent/Day16Test.kt
@@ -8,29 +8,55 @@ class Day16Test {
 
     private val problem = Day16()
     private val input = readInputFile("16").joinToString("\n")
-    private val notes =
-        """
-            class: 1-3 or 5-7
-            row: 6-11 or 33-44
-            seat: 13-40 or 45-50
-
-            your ticket:
-            7,1,14
-
-            nearby tickets:
-            7,3,47
-            40,4,50
-            55,2,20
-            38,6,12
-        """.trimIndent()
 
     @Test
     fun validateFirstSampleInputs() {
+        val notes =
+            """
+                class: 1-3 or 5-7
+                row: 6-11 or 33-44
+                seat: 13-40 or 45-50
+                
+                your ticket:
+                7,1,14
+                
+                nearby tickets:
+                7,3,47
+                40,4,50
+                55,2,20
+                38,6,12
+             """.trimIndent()
+
         assertEquals(71, problem.solveFirst(notes))
     }
 
     @Test
     fun validateFirstSolution() {
         assertEquals(23_009, problem.solveFirst(input))
+    }
+
+    @Test
+    fun validateSecondSampleInputs() {
+        val notes =
+            """
+                departure class: 0-1 or 4-19
+                departure row: 0-5 or 8-19
+                seat: 0-13 or 16-19
+                
+                your ticket:
+                11,12,13
+                
+                nearby tickets:
+                3,9,18
+                15,1,5
+                5,14,9
+             """.trimIndent()
+
+        assertEquals(132, problem.solveSecond(notes))
+    }
+
+    @Test
+    fun validateSecondSolution() {
+        assertEquals(10_458_887_314_153, problem.solveSecond(input))
     }
 }
