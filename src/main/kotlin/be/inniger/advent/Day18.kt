2e2ef9c63e001a9f4f8aac0c7ae4305545a2ff84
Bram Inniger <bram@inniger.be>
2020-12-18 20:45:18 +0200

    D18P02 - Solved

--- src/main/kotlin/be/inniger/advent/Day18.kt
+++ src/main/kotlin/be/inniger/advent/Day18.kt
@@ -1,54 +1,67 @@
 package be.inniger.advent
 
+@Suppress("ComplexRedundantLet")
 class Day18 {
 
     fun solveFirst(expressions: List<String>) =
-        expressions.map { it.replace(" ", "") }.map { eval(it) }.sum()
+        expressions.map { it.replace(" ", "") }.map { eval(it, setOf("+", "*"), setOf()).toLong() }.sum()
 
-    private fun eval(expression: String): Long {
-        var all = mutableListOf<String>()
+    fun solveSecond(expressions: List<String>) =
+        expressions.map { it.replace(" ", "") }.map { eval(it, setOf("+"), setOf("*")).toLong() }.sum()
+
+    private fun eval(expression: String, first: Set<String>, second: Set<String>) =
+        tokenize(expression, first, second)
+            .let { resolveOperations(first, it) }
+            .let { resolveOperations(second, it) }
+            .single()
+
+    private fun resolveOperations(operations: Set<String>, tokens: List<String>): List<String> {
+        return if (operations.none { tokens.contains(it) }) tokens
+        else {
+            val toResolve = operations.map { it to tokens.indexOf(it) }
+                .filter { it.second != -1 }
+                .minByOrNull { it.second }!!
+            val plus = toResolve.second
+            val toInsert = when (val operation = toResolve.first) {
+                "+" -> tokens[plus - 1].toLong() + tokens[plus + 1].toLong()
+                "*" -> tokens[plus - 1].toLong() * tokens[plus + 1].toLong()
+                else -> error("Invalid operation: $operation")
+            }
+
+            val newTokens = tokens.subList(0, plus - 1) + toInsert.toString() + tokens.subList(plus + 2, tokens.size)
+
+            resolveOperations(operations, newTokens)
+        }
+    }
+
+    private fun tokenize(expression: String, first: Set<String>, second: Set<String>): List<String> {
+        val tokens = mutableListOf<String>()
         var index = 0
 
         while (index < expression.length) {
             val value = expression[index]
             when {
                 value.isDigit() || value == '+' || value == '*' -> {
-                    all.add(value.toString())
+                    tokens.add(value.toString())
                     index++
                 }
                 value == '(' -> {
                     val closingParen = findClosingParen(expression.substring(index))
-                    all.add(eval(expression.substring(index + 1, index + closingParen)).toString())
+                    tokens += eval(expression.substring(index + 1, index + closingParen), first, second)
                     index += closingParen + 1
                 }
                 else -> error("Trying to parse invalid value: $value")
             }
         }
 
-        var answer = all[0].toLong()
-        all = all.subList(1, all.size)
-
-        while (all.isNotEmpty()) {
-            val operation = all[0]
-            val nextValue = all[1].toLong()
-
-            when (operation) {
-                "+" -> answer += nextValue
-                "*" -> answer *= nextValue
-                else -> error("Unsupported operation: $operation")
-            }
-
-            all = all.subList(2, all.size)
-        }
-
-        return answer
+        return tokens
     }
 
     private fun findClosingParen(expression: String): Int {
         var openBrackets = 0
 
         for (index in expression.indices) {
-            if (expression[index] == '(') openBrackets ++
+            if (expression[index] == '(') openBrackets++
             else if (expression[index] == ')') {
                 if (openBrackets == 1) return index
                 else openBrackets--
--- src/test/kotlin/be/inniger/advent/Day18Test.kt
+++ src/test/kotlin/be/inniger/advent/Day18Test.kt
@@ -15,12 +15,27 @@ class Day18Test {
         assertEquals(51, problem.solveFirst(listOf("1 + (2 * 3) + (4 * (5 + 6))")))
         assertEquals(26, problem.solveFirst(listOf("2 * 3 + (4 * 5)")))
         assertEquals(437, problem.solveFirst(listOf("5 + (8 * 3 + 9 + 3 * 4 * 3)")))
-        assertEquals(12240, problem.solveFirst(listOf("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")))
-        assertEquals(13632, problem.solveFirst(listOf("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")))
+        assertEquals(12_240, problem.solveFirst(listOf("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")))
+        assertEquals(13_632, problem.solveFirst(listOf("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")))
     }
 
     @Test
     fun validateFirstSolution() {
         assertEquals(3_348_222_486_398, problem.solveFirst(input))
+    }
+
+    @Test
+    fun validateSecondSampleInputs() {
+        assertEquals(231, problem.solveSecond(listOf("1 + 2 * 3 + 4 * 5 + 6")))
+        assertEquals(51, problem.solveSecond(listOf("1 + (2 * 3) + (4 * (5 + 6))")))
+        assertEquals(46, problem.solveSecond(listOf("2 * 3 + (4 * 5)")))
+        assertEquals(1_445, problem.solveSecond(listOf("5 + (8 * 3 + 9 + 3 * 4 * 3)")))
+        assertEquals(669_060, problem.solveSecond(listOf("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")))
+        assertEquals(23_340, problem.solveSecond(listOf("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")))
+    }
+
+    @Test
+    fun validateSecondSolution() {
+        assertEquals(43_423_343_619_505, problem.solveSecond(input))
     }
 }
