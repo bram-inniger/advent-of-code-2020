1fe4679c208d5016f11c7b2d3ff793f39409f12f
Bram Inniger <bram@inniger.be>
2020-12-24 10:16:41 +0200

    D24P02 - Solved

--- src/main/kotlin/be/inniger/advent/Day24.kt
+++ src/main/kotlin/be/inniger/advent/Day24.kt
@@ -3,16 +3,21 @@ package be.inniger.advent
 class Day24 {
 
     fun solveFirst(instructions: List<String>) =
+        parseBlackTiles(instructions).count()
+
+    fun solveSecond(instructions: List<String>, nrDays: Int) =
+        flip(parseBlackTiles(instructions), nrDays).count()
+
+    private fun parseBlackTiles(instructions: List<String>) =
         instructions.map { move(it) }
             .groupingBy { it }
             .eachCount()
             .filterValues { it % 2 != 0 }
-            .count()
+            .keys
 
     private tailrec fun move(instruction: String, position: Coordinate = Coordinate(0, 0)): Coordinate =
         if (instruction.isEmpty()) position
         else when {
-            // e, se, sw, w, nw, and ne
             instruction.startsWith("e") -> move(instruction.substring(1), Coordinate(position.x + 2, position.y))
             instruction.startsWith("se") -> move(instruction.substring(2), Coordinate(position.x + 1, position.y - 1))
             instruction.startsWith("sw") -> move(instruction.substring(2), Coordinate(position.x - 1, position.y - 1))
@@ -21,6 +26,36 @@ class Day24 {
             instruction.startsWith("ne") -> move(instruction.substring(2), Coordinate(position.x + 1, position.y + 1))
             else -> error("Invalid instruction: $instruction")
         }
+
+    private tailrec fun flip(blackTiles: Set<Coordinate>, nrDays: Int): Set<Coordinate> =
+        if (nrDays == 0) blackTiles
+        else flip(
+            blackTiles.flatMap { getNeighbours(it) + it }
+                .distinct()
+                .filter {
+                    val blackNeighbours = nrOfBlackNeighbours(blackTiles, it)
+                    val blackRemainsBlack = blackTiles.contains(it) && blackNeighbours in 1..2
+                    val whiteBecomesBlack = !blackTiles.contains(it) && blackNeighbours == 2
+
+                    blackRemainsBlack || whiteBecomesBlack
+                }
+                .toSet(),
+            nrDays - 1
+        )
+
+    private fun nrOfBlackNeighbours(blackTiles: Set<Coordinate>, position: Coordinate): Int =
+        getNeighbours(position)
+            .filter { blackTiles.contains(it) }
+            .count()
+
+    private fun getNeighbours(position: Coordinate) = listOf(
+        Coordinate(position.x + 2, position.y),
+        Coordinate(position.x + 1, position.y - 1),
+        Coordinate(position.x - 1, position.y - 1),
+        Coordinate(position.x - 2, position.y),
+        Coordinate(position.x - 1, position.y + 1),
+        Coordinate(position.x + 1, position.y + 1)
+    )
 
     private data class Coordinate(val x: Int, val y: Int)
 }
--- src/test/kotlin/be/inniger/advent/Day24Test.kt
+++ src/test/kotlin/be/inniger/advent/Day24Test.kt
@@ -40,4 +40,32 @@ class Day24Test {
     fun validateFirstSolution() {
         assertEquals(512, problem.solveFirst(input))
     }
+
+    @Test
+    fun validateSecondSampleInputs() {
+        assertEquals(15, problem.solveSecond(instructions, 1))
+        assertEquals(12, problem.solveSecond(instructions, 2))
+        assertEquals(25, problem.solveSecond(instructions, 3))
+        assertEquals(14, problem.solveSecond(instructions, 4))
+        assertEquals(23, problem.solveSecond(instructions, 5))
+        assertEquals(28, problem.solveSecond(instructions, 6))
+        assertEquals(41, problem.solveSecond(instructions, 7))
+        assertEquals(37, problem.solveSecond(instructions, 8))
+        assertEquals(49, problem.solveSecond(instructions, 9))
+        assertEquals(37, problem.solveSecond(instructions, 10))
+        assertEquals(132, problem.solveSecond(instructions, 20))
+        assertEquals(259, problem.solveSecond(instructions, 30))
+        assertEquals(406, problem.solveSecond(instructions, 40))
+        assertEquals(566, problem.solveSecond(instructions, 50))
+        assertEquals(788, problem.solveSecond(instructions, 60))
+        assertEquals(1106, problem.solveSecond(instructions, 70))
+        assertEquals(1373, problem.solveSecond(instructions, 80))
+        assertEquals(1844, problem.solveSecond(instructions, 90))
+        assertEquals(2208, problem.solveSecond(instructions, 100))
+    }
+
+    @Test
+    fun validateSecondSolution() {
+        assertEquals(4_120, problem.solveSecond(input, 100))
+    }
 }
